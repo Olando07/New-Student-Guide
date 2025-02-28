8c3f0f070f3168df7ff17aee993518dd19081215
Olando McDonald <olandomcdonald07@gmail.com>
2025-02-28 09:36:26 -0600

    hover and active page effects updated

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,12 @@
 import "../css/navbar.css";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 function NavBar() {
     const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
     const [isTransitioning, setIsTransitioning] = useState(false); // Track transition state
+
+    const { pathname } = useLocation();
 
     const toggleTheme = () => {
         setIsTransitioning(true); // Start transition
@@ -14,7 +16,7 @@ function NavBar() {
     useEffect(() => {
         if (isTransitioning) {
             setTimeout(() => {
-                setIsTransitioning(false)
+                setIsTransitioning(false);
             }, 500);
         }
         document.body.className = theme;
@@ -28,10 +30,14 @@ function NavBar() {
             </div>
             <ul className="navbar-links">
                 <li className="nav-link" id="Home">
-                    <Link to="/New-Student-Guide/">Home</Link>
+                    <Link className={pathname === "/New-Student-Guide/" ? "activeTab" : ""} to="/New-Student-Guide/">
+                        Home
+                    </Link>
                 </li>
                 <li className="nav-link" id="Bibliography">
-                    <Link to="/New-Student-Guide/Bibliography">Bibliography</Link>
+                    <Link className={pathname === "/New-Student-Guide/Bibliography" ? "activeTab" : ""} to="/New-Student-Guide/Bibliography">
+                        Bibliography
+                    </Link>
                 </li>
             </ul>
             <div>
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,23 +1,35 @@
 import "../css/services.css";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 function Service() {
+    const { pathname } = useLocation();
+
     return (
         <ul className="services">
             <li>
-                <Link to="/New-Student-Guide/International-Services">International Services</Link>
+                <Link className={pathname === "/New-Student-Guide/International-Services" ? "active" : ""} to="/New-Student-Guide/International-Services">
+                    International Services
+                </Link>
             </li>
             <li>
-                <Link to="/New-Student-Guide/Academic-Success">Academic Success</Link>
+                <Link className={pathname === "/New-Student-Guide/Academic-Success" ? "active" : ""} to="/New-Student-Guide/Academic-Success">
+                    Academic Success
+                </Link>
             </li>
             <li>
-                <Link to="/New-Student-Guide/Student-Employment">Student Employment</Link>
+                <Link className={pathname === "/New-Student-Guide/Student-Employment" ? "active" : ""} to="/New-Student-Guide/Student-Employment">
+                    Student Employment
+                </Link>
             </li>
             <li>
-                <Link to="/New-Student-Guide/Counseling-Services">Counseling Services</Link>
+                <Link className={pathname === "/New-Student-Guide/Counseling-Services" ? "active" : ""} to="/New-Student-Guide/Counseling-Services">
+                    Counseling Services
+                </Link>
             </li>
             <li>
-                <Link to="/New-Student-Guide/Diversity-on-Campus">Diversity on Campus</Link>
+                <Link className={pathname === "/New-Student-Guide/Diversity-on-Campus" ? "active" : ""} to="/New-Student-Guide/Diversity-on-Campus">
+                    Diversity on Campus
+                </Link>
             </li>
         </ul>
     );
