d138b373634016ff968405f4fee929b52cc4c74f
Olando McDonald <olandomcdonald07@gmail.com>
2025-03-27 17:01:53 -0500

    removing basename

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <BrowserRouter basename="/New-Student-Guide">
+        <BrowserRouter>
             <App />
         </BrowserRouter>
     </StrictMode>
